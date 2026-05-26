b7895edfb216d59037ca5519ed386f32db0b5aae
danie1023 <kelvindaniel1022@gmail.com>
2026-05-26 16:00:00 +0100

    Refactor static file serving in index.js
    Simplify static file serving and remove fallback route.

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
